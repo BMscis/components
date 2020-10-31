59cb4bc0a1384208e9e6fffc780c4e6f7afc24db
BMscis <melvinwafula@gmail.com>
2020-10-31 04:39:42 +0300

    new site for espii

---
+++ README.md
@@ -0,0 +1,12 @@
+npm install --save-dev webpack-dev-server webpack webpack-cli style-loader css-loader file-loader csv-loader xml-loader toml yamljs json5 html-webpack-plugin webpack-manifest-plugin sass-loader webpack-merge sass
+
+npm install --save lodash
+
+//npm install -D babel-loader @babel/core @babel/preset-env
+
+npm install -g sass
+
+npm install @svgdotjs/svg.js
+or
+npm install animejs --save-dev
+
---
+++ webpack.common.js
@@ -0,0 +1,95 @@
+const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const Manifest = require('webpack-manifest-plugin');
+
+module.exports = {
+    entry: {
+        context: path.resolve(__dirname,'src'),
+        app: './src/index.js'
+    },
+    module: {
+        rules: [
+
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                    // Creates `style` nodes from JS strings
+                    'style-loader',
+                    // Translates CSS into CommonJS
+                    'css-loader',
+                    // Compiles Sass to CSS
+                    'sass-loader',
+                ],
+            },
+            {
+                test: /\.(png|svg|jpg|gif)$/,
+                use: [
+                    'file-loader',
+                ],
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/,
+                use: [
+                    'file-loader',
+                ],
+            },
+            {
+                test: /\.(csv|tsv)$/,
+                use: [
+                    'csv-loader',
+                ],
+            },
+            {
+                test: /\.xml$/,
+                use: [
+                    'xml-loader',
+                ],
+            },
+            // {
+            //     test: /\.toml$/,
+            //     type: 'json',
+            //     parser: {
+            //         parse: toml.parse
+            //     }
+            // },
+            // {
+            //     test: /\.yaml$/,
+            //     type: 'json',
+            //     parser: {
+            //         parse: yaml.parse
+            //     }
+            // },
+            // {
+            //     test: /\.json5$/,
+            //     type: 'json',
+            //     parser: {
+            //         parse: json5.parse
+            //     }
+            // }
+        ],
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            title: 'Home',
+        }),
+    ],
+    output: {
+        filename: '[name].[contenthash].js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    optimization: {
+        moduleIds: 'hashed',
+        runtimeChunk: 'single',
+        splitChunks: {
+            cacheGroups: {
+                vendor: {
+                    test: /[\\/]node_modules[\\/]/,
+                    name: 'vendors',
+                    chunks: 'all',
+                },
+            },
+        },
+    },
+};
