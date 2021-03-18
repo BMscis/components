35e3bb94dafab0475abc510da1cfb6643719639e
BMscis <melvinwafula@gmail.com>
2021-03-19 02:47:53 +0300

    Website moved to docs

--- webpack.common.js
+++ webpack.common.js
@@ -102,7 +102,7 @@ module.exports = {
     ],
     output: {
         filename: '[name].[contenthash].js',
-        path: path.resolve(__dirname, 'dist'),
+        path: path.resolve(__dirname, 'docs'),
     },
     optimization: {
         moduleIds: 'deterministic',
--- webpack.dev.js
+++ webpack.dev.js
@@ -5,7 +5,7 @@ module.exports = merge(common, {
   mode: 'development',
   devtool: 'inline-source-map',
   devServer: {
-    contentBase: './dist',
+    contentBase: './docs',
     watchContentBase: true,
     overlay: true,
     hot:true,
